4ae0cc6ba7a10ac2e0c9c1e873c0ed4705fcc0cc
adriancleung <adrian.leung@ualberta.ca>
2019-10-03 17:27:52 -0600

    Added Shape class

---
+++ LabParticipationExercise4/app/src/main/java/com/example/labparticipationexercise4/Shape.java
@@ -0,0 +1,11 @@
+package com.example.labparticipationexercise4;
+
+abstract public class Shape {
+    private int x;
+    private int y;
+
+    public Shape(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
