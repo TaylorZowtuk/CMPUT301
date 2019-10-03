3e197bbf4fe25e4cb0378b6f1de0b79859a3cb32
adriancleung <adrian.leung@ualberta.ca>
2019-10-03 17:35:11 -0600

    Modified Shape Class

--- LabParticipationExercise4/app/src/main/java/com/example/labparticipationexercise4/Shape.java
+++ LabParticipationExercise4/app/src/main/java/com/example/labparticipationexercise4/Shape.java
@@ -1,11 +1,6 @@
 package com.example.labparticipationexercise4;
 
-abstract public class Shape {
+public abstract class Shape {
     private int x;
     private int y;
-
-    public Shape(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
 }
