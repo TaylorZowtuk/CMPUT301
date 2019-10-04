b77c3ee8638419c72fee23fc6f4e2c22daf8a995
Fatih <akfatih2@gmail.com>
2019-10-03 19:06:58 -0600

    My Green Color

--- LabParticipationExercise4/app/src/main/java/com/example/labparticipationexercise4/Shape.java
+++ LabParticipationExercise4/app/src/main/java/com/example/labparticipationexercise4/Shape.java
@@ -3,7 +3,7 @@ package com.example.labparticipationexercise4;
 public abstract class Shape {
     private int x;
     private int y;
-    private String color = "Brown";
+    private String color = "GREEN";
 
     public Shape(int x, int y) {
         this.x = x;
