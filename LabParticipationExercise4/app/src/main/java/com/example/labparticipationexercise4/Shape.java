f5baab6fced72249d2a4231d28242417290b10e3
Grayson Germsheid <germshei@ualberta.ca>
2019-10-03 19:11:31 -0600

    Added color field to Shape.java

--- LabParticipationExercise4/app/src/main/java/com/example/labparticipationexercise4/Shape.java
+++ LabParticipationExercise4/app/src/main/java/com/example/labparticipationexercise4/Shape.java
@@ -3,6 +3,7 @@ package com.example.labparticipationexercise4;
 public abstract class Shape {
     private int x;
     private int y;
+    private String color = "Red";
 
     public Shape(int x, int y) {
         this.x = x;
