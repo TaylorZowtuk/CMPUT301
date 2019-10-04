513641002d39a56142048fb5a2a06ed5db757fb7
David Herczeg <david.herczeg@live.ca>
2019-10-03 18:58:11 -0600

    Modify shape colour to green

--- LabParticipationExercise4/app/src/main/java/com/example/labparticipationexercise4/Shape.java
+++ LabParticipationExercise4/app/src/main/java/com/example/labparticipationexercise4/Shape.java
@@ -3,6 +3,7 @@ package com.example.labparticipationexercise4;
 abstract public class Shape {
     private int x;
     private int y;
+    private String color = "Green";
 
     public Shape(int x, int y) {
         this.x = x;
